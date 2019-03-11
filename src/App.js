08fa6730657bff17e43012f04e33a9fb3d97b44c
AD\yros <pis2me@hotmail.com>
2019-03-11 21:32:11 +0000

    Add state to the App component and pass state to UserOutput component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    userOutput: [
+      { username: 'White', genre: 'FPS' },
+      { username: 'Green', genre: 'RTS' },
+      { username: 'Red', genre: 'MMO'}
+    ], 
+    otherState : 'some other value'
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <p>App main component.</p>
-        <UserOutput username='Black' genre='sport' />
-        <UserOutput username='Yellow' genre='RPG'/>
-        <UserOutput username='Purple' genre='Strategy'/>
+        <UserOutput username={this.state.userOutput[0].username} 
+                    genre={this.state.userOutput[0].genre} />
+        <UserOutput username={this.state.userOutput[1].username} 
+                    genre={this.state.userOutput[1].genre}/>
+        <UserOutput username={this.state.userOutput[2].username}
+                    genre={this.state.userOutput[2].genre}/>
       </div>
     );
   }
