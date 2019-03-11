2b54dbb40bb9883fa74717f59478f0465d2977c6
AD\yros <pis2me@hotmail.com>
2019-03-11 21:11:29 +0000

    Create two stateless compoents and liked the to App root component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Plain main component.</p>
+        <p>App main component.</p>
+        <UserOutput />
+        <UserOutput />
+        <UserOutput />
+
       </div>
     );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>My username is Black and I like Sport video games.</p>
+            <p>My username is Yellow and I like RPG video games.</p>
+        </div>
+    )
+};
+
+export default userOutput;
