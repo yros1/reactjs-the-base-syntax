1b1f5731a24fccb6ae2cc8dba0fdbdfdb98f9489
AD\yros <pis2me@hotmail.com>
2019-03-11 22:34:17 +0000

    Added button which binding example, inline style attached

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   state = {
     userOutput: [
       { username: 'White', genre: 'FPS' },
-      { username: 'Green', genre: 'RTS' },
+      { username: 'Black', genre: 'RTS' },
       { username: 'Red', genre: 'MMO'}
     ], 
     otherState : 'some other value'
@@ -26,26 +26,44 @@ class App extends Component {
   usernameChangedHandler = (event) => {
     this.setState({
       userOutput: [
-        { username: 'Pink', genre: 'FPS' },
+        { username: 'White', genre: 'FPS' },
         { username: event.target.value, genre: 'Sport' },
         { username: 'Red', genre: 'MMO'}
       ] 
     })
   }
   render() {
+
+    const buttonInlineStyle = {
+      backgroundColor: '#4CAF50',
+      border: '2px solid blue',
+      color: 'white',
+      padding: '15px 32px',
+      textalign: 'center',
+      textdecoration: 'none',
+      display: 'inline-block',
+      fontsize: '16px',
+      margin: '4px 2px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <p>App main component.</p>
         <UserInput username={this.state.userOutput[1].username} 
                    changed={this.usernameChangedHandler}>input component</UserInput>
-
-
         <UserOutput username={this.state.userOutput[0].username} 
                     genre={this.state.userOutput[0].genre} />
         <UserOutput username={this.state.userOutput[1].username} 
                     genre={this.state.userOutput[1].genre}/>
         <UserOutput username={this.state.userOutput[2].username}
                     genre={this.state.userOutput[2].genre}/>
+        <button style = {buttonInlineStyle}
+                onClick={this.switchUserNameHandler.bind(this, 'Brown')}> Switch Username</button>
+
+                {/* this is a one way of binding */}
+        {/* <button style = {buttonInlineStyle}
+                onClick={() => this.switchUserNameHandler('Brown')}> Switch Username</button> */}
       </div>
     );
   }
